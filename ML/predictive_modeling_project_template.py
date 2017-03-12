fb9bceaecfa4c1bd5da14c048c42fcc292dec084
brentzucker <brentzucker@gmail.com>
2017-03-12 12:20:36 -0400

    ch 18, Predictive Modeling Project Template

---
+++ ML/predictive_modeling_project_template.py
@@ -0,0 +1,47 @@
+# Ch 18
+# Predictive Modeling Project Template
+
+# 1. Define Problem
+# 2. Summarize Data
+# 3. Prepare Data
+# 4. Evaluate Algorithms
+# 5. Improve Algorithms
+# 6. Present Results
+
+# Python Project Template
+
+
+# 1. Prepare Problem
+# a) Load libraries
+# b) Load dataset (reduce dataset if necessary, model should build in ~30 sec.. always can scale up later)
+
+# 2. Summarize Data
+# a) Descriptive statistics (summaries)
+# b) Data visualizations
+
+# 3. Prepare Data (start simple, revisit and cycle with next step until algorithm and presentation of data is accurate enough)
+# a) Data Cleaning (remove duplicates, mark/impute missing values)
+# b) Feature Selection (remove redundant features, develop new features)
+# c) Data Transforms (scale/redistribute attributes to best expose structure of problem)
+
+# 4. Evaluate Algorithms
+# a) Split-out validation dataset
+# b) Test options and evaluation metric (cross validation and evaluation metric)
+# c) Spot-Check Algorithms
+# d) Compare Algorithms
+
+# 5. Improve Accuracy
+# a) Algorithm Tuning (search for a combination of parameters for each alg that yields the best results)
+# b) Ensembles (combine prediction of multiple models into an ensemble prediction)
+
+# 6. Finalize Model
+# a) Predictions on validation dataset
+# b) Create standalone model on entire training dataset
+# c) Save model for later use
+
+# Tips
+# 1. Fast First pass: go through project as fast as possible. this will give confidence that all the parts are there and a baseline to improve on
+# 2. Cycles: loop through 3-4, 3-4-5
+# 3. Attempt every step
+# 4. Ratchet Accuracy: treat changes as experiements with goal to increase accuracy
+# 5. Adapt as Needed: blur edges of tasks such as 4-5 to beset serve model accuracy
