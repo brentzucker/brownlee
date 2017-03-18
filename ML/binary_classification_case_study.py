0c7fb05ff4783bc126545a95134918e7a2d0abc7
Brent Zucker <brentzucker@gmail.com>
2017-03-18 14:29:37 -0400

    working on ch 21, up to 21.6

--- ML/binary_classification_case_study.py
+++ ML/binary_classification_case_study.py
@@ -67,4 +67,46 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 cax = ax.matshow(dataset.corr(), vmin=-1, vmax=1, interpolation='none')
 fig.colorbar(cax)
+# plt.show()
+
+# 4. Validation Dataset
+array = dataset.values
+X = array[:,0:60].astype(float)
+Y = array[:,60]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = cross_validation.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# 5. Evaluate Algorithms: Baseline
+## Test options and evaluation metric
+num_folds = 10
+num_instances = len(X_train)
+seed = 7
+scoring = 'accuracy'
+
+## Spot Check Algorithms
+models = [] 
+models.append(('LR', LogisticRegression())) 
+models.append(('LDA', LinearDiscriminantAnalysis())) 
+models.append(('KNN', KNeighborsClassifier())) 
+models.append(('CART', DecisionTreeClassifier())) 
+models.append(('NB', GaussianNB())) 
+models.append(('SVM', SVC()))
+
+results = []
+names = []
+for name, model in models:
+    kfold = cross_validation.KFold(n=num_instances, n_folds=num_folds, random_state=seed)
+    cv_results = cross_validation.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+# Visual Comparison of Algorithms 
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
 plt.show()
